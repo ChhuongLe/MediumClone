31be7b5e79436031d692be5a13c9f2e474f7f355
Chhuong Le <chhuongle@gmail.com>
2022-10-31 17:43:02 -0700

    start banner and added css changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,16 +8,19 @@ function Header() {
         <Link href ="/">
           <img className={style.headerImage} src="https://links.papareact.com/yvf"/>
         </Link>
+      </div>
+      <div className={style.container}>
         <div className={style.tab1}>
-          <h3>About </h3>
-          <h3> Contact </h3>
-          <h3 className={style.followTag}> Follow </h3>
+            <p>Our Story</p>
+            <p>Membership</p>
+            <p> Write </p>
+        </div>
+        <div className={style.tab2}>
+          <p>Sign In</p>
+          <p className={style.gettingStartedTag}>Get Started</p>
         </div>
       </div>
-      <div className={style.tab2}>
-        <h3>Sign In</h3>
-        <h3 className={style.gettingStartedTag}>Get Started</h3>
-      </div>
+
     </header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,18 @@ import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div className={styles}>
+    <div className="">
       <Head>
-        <title>Medium Blog</title>
+        <title>Medium Blog Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+      <div className={styles.banner}>
+        <div>
+          <h2 className={styles.bannerHeader}>Stay curious.</h2>
+        </div>
+      </div>
     </div>
   )
 }
