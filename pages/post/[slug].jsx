b33424cf457f320566799916eddf50fc8e6dfaa8
Chhuong Le <chhuongle@gmail.com>
2022-11-07 17:02:23 -0800

    update rendering method to server side rendering

--- pages/post/[slug].jsx
+++ pages/post/[slug].jsx
@@ -1,5 +1,34 @@
+import { sanityClient, urlFor } from '../sanity';
+import 
+
 function Post() {
-  return <div></div>
+  return <main>
+    <Header />
+  </main>
 }
 
 export default Post;
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    slug {
+    current
+    }
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  const paths = posts.map(post => ({
+    params: {
+      slug: post.slug.current
+    }
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps
