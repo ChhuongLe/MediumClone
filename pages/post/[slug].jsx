1f01463edc3deff7f4e02676016f578c24b0ff58
Chhuong Le <chhuongle@gmail.com>
2022-11-18 09:44:18 -0800

    add error handling to form fields

--- pages/post/[slug].jsx
+++ pages/post/[slug].jsx
@@ -7,66 +7,93 @@ import { ErrorMessage } from '@hookform/error-message';
 
 function Post(props) {
   const { register, handleSubmit, formState: {errors} } = useForm();
-  // onSubmit = data => console.log(data);
+
+  const onSubmit = (data) => {
+    console.log("does this work")
+    console.log(data);
+  };
 
   const post = props.post;
-  return <main className={styles.postContainer}>
-    <Header />
-    <img className={styles.mainImage} src={urlFor(post.mainImage).url()} alt=""/>
-    <div className={styles.postContent}>
-      <article className={styles.articleContainer}>
-        <h1 style={{fontSize:"50px", fontWeight:"400", marginBottom:"-5px"}}>{post.title}</h1>
-        <h3 className={styles.postDescription}>{post.description}</h3>
-      <div className={styles.authorContainer}>
-        <img style={{width: "50px", borderRadius:"50%", marginRight:"-10px", marginBottom:"30px"}} src={urlFor(post.author.image).url()}/>
-        <p className={styles.authorInfo}>
-          Blog post by {"    "}
-          <span className={styles.authorName}>{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString()}
-        </p>
+  return (
+    <main className={styles.postContainer}>
+      <Header />
+      <img className={styles.mainImage} src={urlFor(post.mainImage).url()} alt=""/>
+      <div className={styles.postContent}>
+        <article className={styles.articleContainer}>
+          <h1 style={{fontSize:"50px", fontWeight:"400", marginBottom:"-5px"}}>{post.title}</h1>
+          <h3 className={styles.postDescription}>{post.description}</h3>
+        <div className={styles.authorContainer}>
+          <img style={{width: "50px", borderRadius:"50%", marginRight:"-10px", marginBottom:"30px"}} src={urlFor(post.author.image).url()}/>
+          <p className={styles.authorInfo}>
+            Blog post by {"    "}
+            <span className={styles.authorName}>{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+        <div>
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+            content={post.body}
+          />
+        </div>
+        </article>
       </div>
-      <div>
-        <PortableText
-          dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
-          projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
-          content={post.body}
-        />
+      <hr style={{marginLeft: "200px", marginRight: "200px", border:"#ffc017 2px solid"}}/>
+      <div style={{marginLeft:"20%", marginRight:"20%"}}>
+        <h3 style={{color: "#ffc017 ", fontSize: "16px", marginBottom: "-20px"}}>Enjoyed the article?</h3>
+        <h1>Leave a comment below!</h1>
+        <hr style={{border: "1px solid rgba(90, 90, 90, 0.1)", marginTop:"-15px"}}/>
       </div>
-      </article>
-    </div>
-    <hr style={{marginLeft: "200px", marginRight: "200px", border:"#ffc017 2px solid"}}/>
-    <div style={{marginLeft:"20%", marginRight:"20%"}}>
-      <h3 style={{color: "#ffc017 ", fontSize: "16px", marginBottom: "-20px"}}>Enjoyed the article?</h3>
-      <h1>Leave a comment below!</h1>
-      <hr style={{border: "1px solid rgba(90, 90, 90, 0.1)", marginTop:"-15px"}}/>
-    </div>
-    <form className={styles.formContainer}>
-      <input
-       {...register("id", { required: true })}
-       aria-invalid={errors.id ? "true" : "false"}
-        type="hidden" name="_id"
-        value={post.id} />
-      <label className={styles.labelStyle}>
-        <span className={styles.commentSpanStyle}>Name:</span> <br />
-        <input {...register("name", {required: true})}
-          className={styles.inputStyle}
-          placeholder="Name..."
-          type="text"
-        />
-      </label>
-      <label className={styles.labelStyle}>
-        <span className={styles.commentSpanStyle}>Email:</span> <br />
-        <input {...register("email", {required: "true"})}
-          className={styles.inputStyle}
-          placeholder="Email..."
-          type="text"
-        />
-      </label>
-      <label className={styles.labelStyle}>
-        <span className={styles.commentSpanStyle}>Comment:</span> <br />
-        <textarea {...register("comment", {required: true})} className={styles.inputStyle} placeholder="Comment..." type="text" rows={8}/>
-      </label>
-    </form>
+      <form  onSubmit={handleSubmit(onSubmit)} className={styles.formContainer}>
+        {/*Hidden parameter that embeds an id that is specific to the current page*/}
+        <input
+        {...register("id", { required: true })}
+        aria-invalid={errors.id ? "true" : "false"}
+          type="hidden" name="_id"
+          value={post.id} />
+
+        <label className={styles.labelStyle}>
+          <span className={styles.commentSpanStyle}>Name:</span> <br />
+          <input {...register("name", {required: true})}
+            className={styles.inputStyle}
+            placeholder="Name..."
+            type="text"
+          />
+        </label>
+        <label className={styles.labelStyle}>
+          <span className={styles.commentSpanStyle}>Email:</span> <br />
+          <input {...register("email", {required: "true"})}
+            className={styles.inputStyle}
+            placeholder="Email..."
+            type="text"
+            type="email"
+          />
+        </label>
+        <label className={styles.labelStyle}>
+          <span className={styles.commentSpanStyle}>Comment:</span> <br />
+          <textarea {...register("comment", {required: true})}
+            className={styles.inputStyle}
+            placeholder="Comment..."
+            type="text"
+            rows={8}
+          />
+        </label>
+        {/* Return errors when validation fails */}
+        <div style={{display: "flex", flexDirection:"column"}}>
+          {errors.name && (
+            <span style={{color: "red"}}>- The name field is requried</span>
+          )}
+          {errors.comment && (
+            <span style={{color: "red"}}>- The comment field is required</span>
+          )}
+          {errors.email && (
+            <span style={{color: "red"}}>- The email field is required</span>
+          )}
+        </div>
+        <input type="submit"/>
+      </form>
   </main>
+  );
 }
 
 export default Post;
